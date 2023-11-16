441e377669b70d44e7ba743c232174d3cea841a6
Nuhu Ibrahim <140070173+Nuhu-Ibrahim1@users.noreply.github.com>
2023-11-16 21:22:40 +0100

    Update chatbot11.py

--- chatbot11.py
+++ chatbot11.py
@@ -10,7 +10,7 @@ import string
 import streamlit as st
 
 # Load the text file and preprocess the data
-with open(sickle cell disease.txt', 'r', encoding='utf-8') as f:
+with open(sickle cell disease.txt, 'r', encoding='utf-8') as f:
     data = f.read().replace('\n', ' ')
 # Tokenize the text into sentences
 sentences = sent_tokenize(data)
